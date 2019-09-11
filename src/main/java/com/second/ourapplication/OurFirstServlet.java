24ecb09bf54fbbe12c39b4dabb2ea3ac6a505e91
Feral <feralpatelj@gmail.com>
2019-09-11 16:39:52 -0400

    first commit

---
+++ src/main/java/com/second/ourapplication/OurFirstServlet.java
@@ -0,0 +1,32 @@
+package com.second.ourapplication;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class OurFirstServlet extends HttpServlet{
+
+	private static int i=0;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String ourString = "Hello";
+		
+		String responseText = ourString + i + "times";
+		
+		resp.getOutputStream().write(responseText.getBytes());
+		
+		i++;
+		
+	}
+	
+
+	
+	
+	
+}
